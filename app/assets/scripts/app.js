5a77a8991136495bbff09796afdda4ea53658fe9
Joel <joelmusicman@gmail.com>
2018-06-01 18:13:09 -0500

    Completed Reveal On Scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+var featureItem = new RevealOnScroll($(".feature-item"),"85%");
+var testimonial = new RevealOnScroll($(".testimonial"), "60%");
